087d31a017da2629463ee7a3ca313da0ef1302fa
Ashok Gelal <ashokgelal@gmail.com>
2020-02-19 09:10:24 -0700

    make some changes

--- src/main/kotlin/controllers/TaskController.kt
+++ src/main/kotlin/controllers/TaskController.kt
@@ -6,6 +6,8 @@ import dev.alpas.ozone.create
 import dev.alpas.routing.Controller
 import com.smoke.st2.entities.Tasks
 import dev.alpas.ozone.latest
+import dev.alpas.validation.min
+import dev.alpas.validation.required
 
 import me.liuwj.ktorm.dsl.*
 import me.liuwj.ktorm.entity.toList
@@ -13,34 +15,73 @@ import me.liuwj.ktorm.entity.toList
 
 class TaskController : Controller() {
     fun index(call: HttpCall) {
+        // you are making 3 rounds to the database
+        /*
         val tasks = Tasks.latest().toList()
         val total = Tasks.count()
         val completed = Tasks.count { it.completed }
+         */
+        val tasks = Tasks.latest().toList()
+        val total = tasks.size
+        val completed = tasks.count { it.completed }
 
         call.render("welcome", mapOf("tasks" to tasks, "total" to total, "completed" to completed ))
     }
 
     fun store(call: HttpCall) {
-        val newTask = call.stringParam("newTask").orAbort()
-        val now = call.nowInCurrentTimezone().toInstant()
-        val post = Tasks.create() {
-            it.name to newTask
-            it.createdAt to now
-            it.updatedAt to now
+        // with this I can create a task with just an empty string. Let's actually validate it first
+
+        call.applyRules("newTask") {
+            required()
+            min(2)
+        }.validate()
+
+        /* val newTask = call.stringParam("newTask").orAbort() */
+
+        // With new createdAt and updatedAt bindings, you don't need this in most of the cases.
+        // If you need more precise control over the timezone then you need it.
+        /* val now = call.nowInCurrentTimezone().toInstant() */
+
+        Tasks.create() {
+            it.name to call.stringParam("newTask")
+            /*
+                it.createdAt to now
+                it.updatedAt to now
+             */
         }
-        flash("success", "Successfully added to-do")
+        flash("success", "Successfully added the task!")
         call.redirect().back()
     }
 
     fun delete(call: HttpCall) {
         val id = call.longParam("id").orAbort()
         Tasks.delete { it.id eq id }
-        flash("success", "Successfully removed to-do")
+        flash("success", "Successfully removed the task!")
         call.redirect().back()
     }
 
     fun update(call: HttpCall) {
         val id = call.longParam("id").orAbort()
+        // Set the name of the checkbox to something like "state" and use it.
+        // I'll explain to you later why this is better.
+        val markAsComplete = call.param("state") != null
+
+        Tasks.update {
+            it.completed to markAsComplete
+            where {
+                it.id eq id
+            }
+        }
+
+        if (markAsComplete) {
+            flash("success", "Successfully completed the task!")
+        } else {
+            flash("success", "Successfully updated the task!")
+        }
+
+        call.redirect().back()
+
+        /*
         val currentState = Tasks.select().where { Tasks.id eq id }.map { row -> row[Tasks.completed] }
 
          if (currentState.first()!!) {
@@ -64,7 +105,7 @@ class TaskController : Controller() {
              flash("success", "Successfully completed to-do")
              call.redirect().back()
         }
-
+         */
     }
 
 
